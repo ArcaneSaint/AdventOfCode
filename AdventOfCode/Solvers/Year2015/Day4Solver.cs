251de2e3f067dd0c8f920eff8c85133ca3608660
Audric <audric.deschaetzen@gmail.com>
2024-12-04 21:57:57 +0100

    day 4 async? bah, even slower

--- AdventOfCode/Solvers/Year2015/Day4Solver.cs
+++ AdventOfCode/Solvers/Year2015/Day4Solver.cs
@@ -7,13 +7,21 @@ internal class Day4Solver(long part1Test = 0, long part2Test = 0) : BaseSolver20
     public override long Part1(string[] input)
     {
         var line = input[0];
-        var result = 0;
-        var hash = "";
-        do
+        var result = Int32.MaxValue;
+        
+        Parallel.For(0, Int32.MaxValue, new ParallelOptions
         {
-            result++;
-            hash = CreateMD5(line + result);
-        } while (!hash.StartsWith("00000"));
+            MaxDegreeOfParallelism = Environment.ProcessorCount
+        },
+        (i, state) =>
+        {
+            if (i > result) { return; }
+            var hash = CreateMD5(line + i);
+            if (hash.StartsWith("00000") && i < result)
+            {
+                result = i;
+            }
+        });
 
         return result;
     }
