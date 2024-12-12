9664833ebcb1280a2d5179c7f04eb242dbb8b7fb
Audric <audric.deschaetzen@gmail.com>
2024-12-12 18:41:27 +0100

    refactor datafile format; created placeholder datafiles for future challenges

--- AdventOfCode/Helpers/DataHelper.cs
+++ AdventOfCode/Helpers/DataHelper.cs
@@ -2,8 +2,8 @@
 
 public static class DataHelper
 {
-    private const string FileFormat = "Data/{0}/Data/{1}.txt";
-    private const string TestFileFormat = "Data/{0}/Test/{1}.txt";
+    private const string FileFormat = "Data/{0}/Data/{1:D2}.txt";
+    private const string TestFileFormat = "Data/{0}/Test/{1:D2}.txt";
     public static string[] GetInput(int year, int day, bool test = false)
     {
         var file = string.Format(test ? TestFileFormat : FileFormat, year, day);
